1007f74d3f2b1dc5d5340c5cee752cf81194c7e1
IVEWOR <howdy@deepslog.com>
2022-05-20 03:30:07 +0530

    test 1

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
+courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+]
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.listen(port, function() {
-  console.log(`Example app listening on port ${port}!`)
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
 });
+
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("Course with given id not found");
+  res.send(course);
+})
+
+
+const port = process.env.PORT || 3000
+app.listen(3000, () => console.log(`Listening to port ${port}....`));
