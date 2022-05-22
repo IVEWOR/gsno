98d3b18af84e55faa4bedd60b730475d6c9d4b08
IVEWOR <howdy@deepslog.com>
2022-05-22 07:37:37 +0530

    a few models setup

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// setup mongoose connection
+var mongoose = require('mongoose');
+var mongoDB =
+  'mongodb+srv://ivewor_db:<ivewor_pass>@cluster0.i1ofr.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// get the default database connection
+var db = mongoose.connection;
+
+// error handling
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -23,12 +35,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
